605c5d67e8b9bce333d4e29b466013db6870c39e
Zasenko <dmitry.zasenko@gmail.com>
2022-12-27 23:38:19 +0100

    EmptyProject

---
+++ MySwiftUI/MySwiftUI/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  MySwiftUI
+//
+//  Created by Dmitry Zasenko on 27.12.22.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
