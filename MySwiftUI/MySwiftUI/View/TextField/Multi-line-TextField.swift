cc0b49fc5c0db26b659bddea086ab8275f87c941
Zasenko <dmitry.zasenko@gmail.com>
2023-02-12 13:23:25 +0100

    add Multiline TextField

---
+++ MySwiftUI/MySwiftUI/View/TextField/Multi-line-TextField.swift
@@ -0,0 +1,33 @@
+//
+//  Multi-line-TextField.swift
+//  MySwiftUI
+//
+//  Created by Dmitry Zasenko on 12.02.23.
+//
+
+import SwiftUI
+
+struct MultilineTextField: View {
+    @State var text = ""
+    @State var email = ""
+    
+    var body: some View {
+        Form {
+          Section("Your details") {
+            TextField("email", text: $email)
+              .textInputAutocapitalization(.never)
+              .autocorrectionDisabled(true)
+          }
+          Section("Feedback") {
+              TextField("text", text: $text, axis: .vertical)
+                .lineLimit(2...4)
+          }
+        }
+    }
+}
+
+struct MultilineTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        MultilineTextField()
+    }
+}
